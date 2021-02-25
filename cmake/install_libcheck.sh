961ff54a6f722f189ac30832be14c1aa4ee551c2
timoniq <52574917+timoniq@users.noreply.github.com>
2021-02-25 16:23:17 +0300

    add libcheck installation to ci

---
+++ cmake/install_libcheck.sh
@@ -0,0 +1,15 @@
+# Check library needed for macro testing
+# https://libcheck.github.io/check/web/install.html
+
+if [[ "$OS" == *"ubuntu"* ]]; then
+  echo "Installing check with Aptitude..."
+  sudo apt-get install check
+elif [[ "$OS" == *"macos"* ]]; then
+  if [[ $(command -v brew) != "" ]]; then
+    brew install check
+  else
+    echo "Brew needs to be installed to build check on OSX"
+  fi
+else
+  echo "Instruction for $OS not found"
+fi
